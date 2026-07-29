7ba06f316776fcc2cff0dd8e2655c8487840d699
ghannam <yousefghannam61@gmail.com>
2026-07-30 00:08:48 +0300

    Refactor PrismaService to clean NestJS DI provider standard

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,22 +1,12 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
-const globalForPrisma = globalThis as unknown as {
-  prisma?: PrismaClient;
-};
-
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
   constructor() {
-    if (globalForPrisma.prisma) {
-      return globalForPrisma.prisma as PrismaService;
-    }
     super({
       log: process.env.NODE_ENV === 'development' ? ['query', 'info', 'warn', 'error'] : ['error'],
     });
-    if (process.env.NODE_ENV !== 'production') {
-      globalForPrisma.prisma = this;
-    }
   }
 
   async onModuleInit() {
